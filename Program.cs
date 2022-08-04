2d9be0d2fa7f43316b6e4e99cd6bd76185a6f30e
Raviprasad sathe <92899647+Satheraviprasad570@users.noreply.github.com>
2022-08-04 10:13:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace filepractice4
+{
+    class Program
+    {
+        public static void streamwriter()
+        {
+            string path = @"C:\Raviprasad\data\streamesample.txt";
+            using(filestream)
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
